b2351ea584e7fa3c205e6534d81d944e80bdcea2
lucas12996 <leelucas12996@gmail.com>
2023-12-27 04:52:28 +0900

    Lucas's Commit

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+    },
+    // TODO: font-awesome set by robert
+    fonts: {
+      body: {
+        family:'Robto',
+      },
+    }
+  },
+  plugins: [],
+};
